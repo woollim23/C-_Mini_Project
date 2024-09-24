5a857408e09cc1d86efdaf530a111ea6efaf8e1b
woollim23 <begin2125@gmail.com>
2024-09-25 01:42:07 +0900

    파일 정리

--- TextRPG_Reform/TextRPG_Reform/obj/Debug/net6.0/TextRPG_Reform.AssemblyInfo.cs
+++ TextRPG_Reform/TextRPG_Reform/obj/Debug/net6.0/TextRPG_Reform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG_Reform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b74bd32ea76413cdbca8b79ee288d60e498f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953f5cd4c23d1dbcc4225602a1bd4847fb085010")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG_Reform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG_Reform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
